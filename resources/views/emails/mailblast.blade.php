305396107ec460225e185711db583e3a6059d757
omar <omargponce@gmail.com>
2021-06-15 10:13:20 -0400

    Mesage de BEA

--- resources/views/emails/mailblast.blade.php
+++ resources/views/emails/mailblast.blade.php
@@ -5,7 +5,7 @@ Saludos,  
 <br>
 Gracias por participar del servicio ofrecido por A+ Education & Consulting Solutions. Adjunto su certificado de participación.
 <br>
-De haber algún error en el certificado, favor notificarlo a beatriz.fernandez@quieroaplus.com. Estamos trabajando por orden de fecha. Si usted participó en varios servicios, recibirá un email por servicio (1 certificado por email).
+De haber algún error en el certificado, favor notificarlo a beatriz.fernandez@quieroaplus.com. Si usted participó en varios servicios, recibirá un email por servicio (1 certificado por email). Nota: Puede ser no reciba todos los certificados el mismo día. Estamos trabajando para que en el mes de Junio 2021 tenga todos los certificados 2020-2021.
 
 Gracias,<br>
 {{ config('app.name') }}
